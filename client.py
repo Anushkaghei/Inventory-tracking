9ffba19505b4c4b4c060c4c3614466b95bf89bda
Anushka Ghei <79694271+Anushkaghei@users.noreply.github.com>
2023-04-24 01:00:01 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -1,31 +1,41 @@
-import socket
 import cv2
+import numpy as np
+import socket
 import pickle
 import struct
 
-# create socket
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-host_ip = ' ' # paste your server ip address here
-port = 9999
-client_socket.connect((host_ip,port)) # a tuple
-data = b""
-payload_size = struct.calcsize("Q")
+# Set up client socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('172.20.10.5', 8888))
+
+# OpenCV setup for video display
+cv2.namedWindow("Video Stream", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Video Stream", 640, 480)
+
+# Loop for receiving and displaying video frames
 while True:
-	while len(data) < payload_size:
-		packet = client_socket.recv(4*1024) # 4K
-		if not packet: break
-		data+=packet
-	packed_msg_size = data[:payload_size]
-	data = data[payload_size:]
-	msg_size = struct.unpack("Q",packed_msg_size)[0]
-	
-	while len(data) < msg_size:
-		data += client_socket.recv(4*1024)
-	frame_data = data[:msg_size]
-	data  = data[msg_size:]
-	frame = pickle.loads(frame_data)
-	cv2.imshow("RECEIVING VIDEO",frame)
-	key = cv2.waitKey(1) & 0xFF
-	if key  == ord('q'):
-		break
+    # Receive frame size
+    frame_size_data = b''
+    while len(frame_size_data) < 4:
+        frame_size_data += client_socket.recv(4 - len(frame_size_data))
+    frame_size = struct.unpack('!I', frame_size_data)[0]
+    
+    # Receive frame data
+    frame_data = b''
+    while len(frame_data) < frame_size:
+        frame_data += client_socket.recv(frame_size - len(frame_data))
+    
+    # Deserialize frame data and extract frame and water level information
+    frame, water_level = pickle.loads(frame_data)
+    
+    # Display received frame
+    cv2.imshow("Video Stream", frame)
+    print("Water Level:", water_level)
+    
+    # Exit loop when 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Close client socket and destroy OpenCV window
 client_socket.close()
+cv2.destroyAllWindows()
